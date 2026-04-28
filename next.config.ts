a65de79ace747f45d987141f882868eb9c9b62ed
Siddharthchauhan <Siddharthchauhan3128@gmail.com>
2026-04-28 20:08:22 +0530

    Initial Nexus MVP submission

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
-  }
+  },
+  output:"export"
 };
 
 export default nextConfig;
